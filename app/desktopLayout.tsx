5501ee4fcf101a0122191974281008d0f10da2ff
ryckmob <oliveirachaves.ch97@gmail.com>
2025-09-22 07:18:00 -0300

    aviso

--- app/desktopLayout.tsx
+++ app/desktopLayout.tsx
@@ -1,10 +1,14 @@
 export default function DesktopLayout() {
   return (
     <div>
-      <h1 className="px-10 py-20 bg-yellow-400 text-2xl">Atenção!</h1>
-      <p className="px-10 mt-5 text-sm">
-      Esta plataforma está otimizada exclusivamente para dispositivos móveis. Estamos desenvolvendo a versão para computadores e em breve ela estará disponível. Agradecemos sua compreensão.
-      </p>
+      <h1 className="px-10 py-20 bg-yellow-400 text-2xl text-center">Atenção!</h1>
+      <div className="flex flex-row px-10 mt-5 gap-3 justify-center">
+        <img src="/tl.webp" alt="" width={80} height={80} />
+        <p className="text-sm w-[600px]">
+        Esta plataforma está otimizada exclusivamente para dispositivos móveis. Estamos desenvolvendo a versão para computadores e em breve ela estará disponível. Agradecemos sua compreensão.
+        </p>
+      </div>
+      
     </div>
   );
 }
